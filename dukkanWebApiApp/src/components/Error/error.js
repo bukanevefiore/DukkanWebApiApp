0534c178202e85a428da5b51348e211c3d7779b0
Melis Nur KILIÇ <fiorebucaneve22@gmail.com>
2021-08-01 12:44:32 +0300

    temizleme

--- dukkanWebApiApp/src/components/Error/error.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-//import LottieView from 'lottie-react-native';
-
-function Error() {
-   // return <LottieView source={require('../../assets/error.json')} autoPlay />;
-}
-
-export default Error;
--- dukkanWebApiApp/src/components/Loading/loading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-//import LottieView from 'lottie-react-native';
-
-function Loading() {
-    //return <LottieView source={require('../../assets/loading.json')} autoPlay />;
-}
-
-export default Loading;
